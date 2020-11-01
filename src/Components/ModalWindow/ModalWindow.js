079b5bfb637e4bd40d86b9f255933f8402b20860
Timofey Chunarev <timofei.chunarev@gmail.com>
2020-11-01 11:18:07 +0700

    refactor: change the modalWindow component
    rewrite the modalWindow component from the class to functional

--- src/Components/ModalWindow/ModalWindow.js
+++ src/Components/ModalWindow/ModalWindow.js
@@ -1,35 +1,22 @@
-import React, {Component} from 'react';
+import React from 'react';
 import Form from '../Form/Form';
 
 import './ModalWindow.css';
 
-export default class ModalWindow extends Component {
+export default function ModalWindow(props) {
 
-    constructor(props) {
-        super(props);
-        this.handleChange = this.handleChange.bind(this);
-    }
-
-    handleChange(e) {
-        this.props.handleInputChange('newItem', e.target.value);
-    }
-
-    render() {
-        return(
-
-            <div className="container container_position">
-                <div className="modal-window">
-                    <Form 
-                        handleChange={this.handleChange}
-                        areaClass={this.props.areaClass} 
-                        text={this.props.text}
-                        onClose={this.props.onClose}
-                        textButton={this.props.textButton}
-                        addItem={this.props.addItemToPage}
+    return(
+        <div className="container container_position">
+            <div className="modal-window">
+                <Form 
+                    handleChange={(e) => props.handleInputChange('newItem', e.target.value)}
+                    areaClass={props.areaClass}
+                    text={props.text}
+                    onClose={props.onClose}
+                    textButton={props.textButton}
+                    addItem={props.addItemToPage}   
                     />
-                </div>
             </div>
-
-        )
-    }
-} 
+        </div>
+    )
+}
